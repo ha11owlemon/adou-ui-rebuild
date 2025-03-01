385d4595d7f0e6db9eefcd1d70575b6e74b67e8f
hallowlemon <11492158+hallowlemon@user.noreply.gitee.com>
2025-03-02 04:39:36 +0800

    fix: 注释掉input demo中的文件

--- src/Input/demo/basic.tsx
+++ src/Input/demo/basic.tsx
@@ -1,18 +1,18 @@
-import { Input } from 'adou-ui-rebuild';
-import React from 'react';
-const VarientExample = () => {
-  return (
-    <div className="varient-wrapper">
-      <div className="outlined-input">
-        <Input placeholder="请输入内容" />
-      </div>
-      <div className="filled-input my-2">
-        <Input size="large" placeholder="请输入内容" />
-      </div>
-      <div className="borderless-input">
-        <Input size="small" placeholder="请输入内容" />
-      </div>
-    </div>
-  );
-};
-export default VarientExample;
+// import { Input } from 'adou-ui-rebuild';
+// import React from 'react';
+// const VarientExample = () => {
+//   return (
+//     <div className="varient-wrapper">
+//       <div className="outlined-input">
+//         <Input placeholder="请输入内容" />
+//       </div>
+//       <div className="filled-input my-2">
+//         <Input size="large" placeholder="请输入内容" />
+//       </div>
+//       <div className="borderless-input">
+//         <Input size="small" placeholder="请输入内容" />
+//       </div>
+//     </div>
+//   );
+// };
+// export default VarientExample;
