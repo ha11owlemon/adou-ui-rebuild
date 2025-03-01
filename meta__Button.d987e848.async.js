e26280367be6c9f30f2d6a4e047619c6d9709608
ha11owlemon <ha11owlemon@users.noreply.github.com>
2025-03-01 18:58:00 +0000

    deploy: f46e7109ee8846e8e70f1cd6323a457184e4eb92

---
+++ meta__Button.d987e848.async.js
@@ -0,0 +1,19 @@
+"use strict";(self.webpackChunkadou_ui_rebuild=self.webpackChunkadou_ui_rebuild||[]).push([[461],{14361:function(d,t,n){var r;n.r(t),n.d(t,{demos:function(){return p}});var s=n(17061),a=n.n(s),h=n(17156),_=n.n(h),u=n(67294),v=n(4497),p={"button-demo-basic":{component:u.memo(u.lazy(function(){return n.e(570).then(n.bind(n,49693))})),asset:{type:"BLOCK",id:"button-demo-basic",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(67887).Z},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u6D4B\u8BD51"},context:{react:r||(r=n.t(u,2))},renderOpts:{compile:function(){var i=_()(a()().mark(function m(){var o,c=arguments;return a()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(o=e.sent).default.apply(o,c));case 3:case"end":return e.stop()}},m)}));function l(){return i.apply(this,arguments)}return l}()}},"button-demo-test1":{component:u.memo(u.lazy(function(){return n.e(570).then(n.bind(n,22128))})),asset:{type:"BLOCK",id:"button-demo-test1",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(20968).Z},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u6D4B\u8BD52"},context:{react:r||(r=n.t(u,2))},renderOpts:{compile:function(){var i=_()(a()().mark(function m(){var o,c=arguments;return a()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(o=e.sent).default.apply(o,c));case 3:case"end":return e.stop()}},m)}));function l(){return i.apply(this,arguments)}return l}()}}}},73120:function(d,t,n){n.r(t),n.d(t,{texts:function(){return s}});var r=n(4497);const s=[{value:"This is an example component.",paraId:0},{value:"\u8FD9\u5C31\u662F\u4E00\u4E2A\u6309\u94AE\u6765\u7740\u3002",paraId:1}]},67887:function(d,t){t.Z=`import React from 'react'
+
+const Basic = () => {
+  return (
+    <div>
+      <h1>Basic</h1>
+    </div>
+  )
+}
+export default Basic`},20968:function(d,t){t.Z=`import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Basic</h1>
+    </div>
+  )
+}
+export default Test`}}]);
---
+++ nm__dumi__dist__client__pages__404.ba0a3c61.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkadou_ui_rebuild=self.webpackChunkadou_ui_rebuild||[]).push([[65],{95558:function(e,u,_){_.d(u,{Z:function(){return d.Z}});var d=_(28413)}}]);
